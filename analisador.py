5e7fe4b4d7c0c16b4e646c2bb54920f67115d250
Gladistony Silva Lins <bistone3000@hotmail.com>
2024-02-02 22:08:00 -0300

    Função ajustada
    Foi feito o ajuste da função para usar algoritmos gulosos

--- analisador.py
+++ analisador.py
@@ -4,18 +4,32 @@ class Analisador:
 
     def acha_sequencia(self, probs, valores) -> int:
         tamanho = len(valores)
-        possibilidades = self.gerar_sequencia(tamanho) 
-        melhorOrdem = possibilidades[0] 
-        melhorResultado = self.calcularTotalGanhos(probs, valores, melhorOrdem)
-        for o in possibilidades:
-            resultado = self.calcularTotalGanhos(probs, valores, o)
-            if resultado > melhorResultado:
-                melhorResultado = resultado
-                melhorOrdem = o
-        return melhorOrdem
+        ordem = []
+        usados = [] 
+        valor_acumulado = 0 
+        prob_acumulada = 1 
+        while len(ordem) < tamanho: 
+            x = self.seleciona(probs, valores, usados) 
+            ordem.append(x) 
+            usados.append(x) 
+            valor_acumulado += valores[x] 
+            prob_acumulada *= probs[x] 
+        return ordem 
 
 
-    def calcularTotalGanhos(self, probs, valores, ordem) -> int:
+    def seleciona(self, probs, valores, usados) -> int:
+        melhor = -1 
+        melhor_valor = 0 
+        for i in range(len(valores)):
+            if i not in usados: 
+                valor = valores[i] * probs[i] * probs[i] 
+                #Em vez de atribuir pesos equivalentes para o valor e a probabilidade, foi dado maior importancia a probabilidade de acerta
+                if valor > melhor_valor: 
+                    melhor = i
+                    melhor_valor = valor
+        return melhor 
+
+    def objetivo(self, ordem, probs, valores) -> float:
         resultado = 0
         for i in range(len(ordem)):
             prod = 1
@@ -32,19 +46,3 @@ class Analisador:
             coef = round(prod*chance, 3)
             resultado += coef * soma
         return round(resultado, 3)
-    def gerar_sequencia(self, tamanho):
-        self.resultados = []
-        self.gerar_sequencias_rec(tamanho)
-        return self.resultados
-    def gerar_sequencias_rec(self, tamanho, sequencia=[], usados=[]):
-        if len(sequencia) == tamanho: 
-            self.resultados.append(sequencia.copy()) 
-        else:
-            for id in range(tamanho): 
-                if id not in usados: 
-                    sequencia.append(id) 
-                    usados.append(id)
-                    self.gerar_sequencias_rec(tamanho, sequencia, usados) 
-                    sequencia.pop() 
-                    usados.remove(id) 
-        return self.resultados 
